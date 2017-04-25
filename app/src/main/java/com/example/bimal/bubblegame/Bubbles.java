eb2d65840bd35b820cc11359816f5a1f9c1cd931
bimal <bimalparajulise@gmail.com>
2017-04-25 17:03:30 -0500

    Bubble game

---
+++ app/src/main/java/com/example/bimal/bubblegame/Bubbles.java
@@ -0,0 +1,107 @@
+package com.example.bimal.bubblegame;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+public class Bubbles extends AppCompatActivity {
+    public static int SPLASH_TIME_OUT = 5000;
+    public static int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bubbles);
+        final Button bubble1, bubble2, bubble3, bubble4, bubble5;
+        final Intent i = new Intent(getApplicationContext(), Win.class);
+        final TextView mTextField = (TextView) findViewById(R.id.textView8);
+
+        bubble1 = (Button) findViewById(R.id.bubble1);
+        bubble2 = (Button) findViewById(R.id.bubble2);
+        bubble3 = (Button) findViewById(R.id.bubble3);
+        bubble4 = (Button) findViewById(R.id.bubble4);
+        bubble5 = (Button) findViewById(R.id.bubble5);
+
+        // Count-Down (show timer)
+        new CountDownTimer(SPLASH_TIME_OUT, 1000) {
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+            public void onFinish() {
+                mTextField.setText("Time Up!");
+            }
+        }.start();
+
+        //        SET TIME FOR GAME TO FINISH WITHIN TIME_OUT
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(counter != 5) {
+                    Intent i2 = new Intent(getApplicationContext(), Lose.class);
+                    startActivity(i2);
+                }
+                counter = 0;
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+        bubble1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                bubble1.setVisibility(v.INVISIBLE);
+                if(counter == 5){
+                    startActivity(i);
+                }
+            }
+        });
+        bubble2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                bubble2.setVisibility(v.INVISIBLE);
+                if(counter == 5){
+                    startActivity(i);
+                }
+            }
+        });
+        bubble3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                bubble3.setVisibility(v.INVISIBLE);
+                if(counter == 5){
+                    startActivity(i);
+                  }
+            }
+        });
+        bubble4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                bubble4.setVisibility(v.INVISIBLE);
+                if(counter == 5){
+                   startActivity(i);
+                }
+            }
+        });
+        bubble5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                bubble5.setVisibility(v.INVISIBLE);
+                if(counter == 5){
+                    startActivity(i);
+                  }
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/bimal/bubblegame/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.bimal.bubblegame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startBtn, level2, level3, level4, level5, level6, level7;
+        startBtn = (Button) findViewById(R.id.start_btn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Bubbles.class);
+                startActivity(i);
+            }
+        });
+
+        level2 = (Button) findViewById(R.id.button2);
+        level2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Level 2 Under Construction",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        level3 = (Button) findViewById(R.id.button3);
+        level3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Level 3 Under Construction",Toast.LENGTH_SHORT).show();
+            }
+        });
+        level4 = (Button) findViewById(R.id.button4);
+        level4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Level 4 Under Construction",Toast.LENGTH_SHORT).show();
+            }
+        });
+        level5 = (Button) findViewById(R.id.button5);
+        level5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Level 5 Under Construction",Toast.LENGTH_SHORT).show();
+            }
+        });
+        level6 = (Button) findViewById(R.id.button6);
+        level6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Level 6 Under Construction",Toast.LENGTH_SHORT).show();
+            }
+        });
+        level7 = (Button) findViewById(R.id.button7);
+        level7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Level 7 Under Construction",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
